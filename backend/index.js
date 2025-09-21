968468d0aa635b177e6fa9e6c00c0595e25ff6c3
aman <04.aman.raj@gmail.com>
2025-09-21 22:40:17 +0530

    feat: user & profile endpoints

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,23 @@ const { requestLogger } = require("./middlewares/logger");
 require("dotenv").config();
 
 const app = express();
-const port=process.env.PORT||3000
+const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 // Default route
-app.get('/',(req,res)=>(
-    res.send("hello world! from backend")
-))
+app.get("/", (req, res) => res.send("hello world! from backend"));
 
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`)
-})
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+app.use("/api/user", require("./routes/user.route.js"));
+app.use("/api/profile", require("./routes/profile.route.js"));
+
+// Start server
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
