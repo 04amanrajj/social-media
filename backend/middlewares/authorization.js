1d7bdc07c55e07cab5571906f494ddae8ccac745
aman <04.aman.raj@gmail.com>
2025-09-21 22:39:26 +0530

    refactor: jwt token auth

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 exports.requireAuth = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Authorization header missing" });
-  }
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ message: "Token missing" });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId; // Attach userId to request object
+    const authHeader = req.headers.authorization;
+    if (!authHeader?.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Authorization header missing" });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Token missing" });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId; // Attach userId to request object
     next();
   } catch (error) {
     console.log("JWT Error:", error);
