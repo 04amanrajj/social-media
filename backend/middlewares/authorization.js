92ed02e8c79348898c53546040e8d21f28366a63
aman <04.aman.raj@gmail.com>
2025-09-22 23:56:05 +0530

    feat: blacklist token validation

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const db = require("../config/db");
 
-exports.requireAuth = (req, res, next) => {
+exports.requireAuth = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader?.startsWith("Bearer ")) {
         return res.status(401).json({ message: "Authorization header missing" });
@@ -10,8 +11,13 @@ exports.requireAuth = (req, res, next) => {
         return res.status(401).json({ message: "Token missing" });
     }
     try {
+      const blacklistedToken = await db("blacklisted_tokens").where({ token }).first();
+      if (blacklistedToken) {
+        return res.status(401).json({ message: "Token has been revoked" });
+      }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId; // Attach userId to request object
+        req.token=token;
     next();
   } catch (error) {
     console.log("JWT Error:", error);
