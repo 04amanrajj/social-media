80d26c8588601382c793ca183afb403ead16f47d
aman <04.aman.raj@gmail.com>
2025-09-28 23:26:22 +0530

    feat: wrap express with socket

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const {initSocket}=require("./config/socket.js");
 const { requestLogger } = require("./middlewares/logger");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 app.use(requestLogger);
 
 // Default route
 app.get("/", (req, res) => res.send("hello world! from backend"));
-
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
+app.get("/health", (req, res) => res.status(200).send("OK"));
 app.use("/api/user", require("./routes/user.route.js"));
 app.use("/api/profile", require("./routes/profile.route.js"));
 app.use("/api/post", require("./routes/post.route.js"));
+app.use("/api/chats", require("./routes/chat.route.js"));
+
+// Socket.IO setup
+const server = http.createServer(app);
+
+initSocket(server);
 
 // Start server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
